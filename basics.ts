4673c358b50565ed1267408b7c992d9019d8546b
Farmasi Attila <attila.farmasi@dpdgroup.com>
2022-12-01 10:14:47 +0100

    Functions and function types

--- basics.ts
+++ basics.ts
@@ -46,3 +46,12 @@ let course: string | number = 'React - The Complete Guide';
 
 course = 12341;
 
+// Functions & types
+
+function add(a: number, b: number) {
+    return a + b;
+};
+
+function printOutput(value: any) {
+    console.log(value);
+}
